bef060e8dd022ec8320d44153ebd18bd8daf01f6
Hiba Dawood <164556977+HibaDawood@users.noreply.github.com>
2026-06-03 18:57:38 +0500

    Refactor PageProps and make Products component async
    Refactored PageProps to use a Promise for params and made the Products component async to handle the resolved params.

--- src/app/[Categorey]/[slug]/page.tsx
+++ src/app/[Categorey]/[slug]/page.tsx
@@ -1,18 +1,14 @@
 import SlugPage from "./slugPage";
 
-// 1. Interface mein params ko Promise bna diya
 interface PageProps {
   params: Promise<{ slug: string }>;
 }
 
-// 2. Component ko 'async' banaya
 const Products = async ({ params }: PageProps) => {
-  // 3. params ko await kar ke resolve kiya
   const resolvedParams = await params;
 
   return (
     <section className="body-font overflow-hidden">
-      {/* 4. resolvedParams ko SlugPage component ke andar pass kar diya */}
       <SlugPage params={resolvedParams}/>
     </section>
   );
